074d1f36180b032281f042cf8e5c3cc726540eea
Lazar Szilard <lazar.szilard1995@gmail.com>
2019-01-13 19:12:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WeatherService } from './weather.service';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,77 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'graphic-app';
+  chart = [];
+
+  constructor(private _weather: WeatherService){}
+
+  ngOnInit() {
+    this._weather.dailyForecast()
+      .subscribe(res => {
+        console.log(res)
+         const temp_max = res['list'].map(x => x.temp_max);
+         const temp_min = res['list'].map(x => x.temp_min);
+         const alldates = res['list'].map(x => x.dt);
+
+        let weatherDates = [];
+        alldates.forEach(element => {
+          let jsdate = new Date(element * 1000);
+          weatherDates.push(jsdate.toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric'}))
+          console.log(weatherDates)
+
+          this.chart = new Chart('canvas', {
+            type: 'bar',
+            data: {
+              labels: weatherDates,
+              dataset: [
+                {
+                  data: temp_max,
+                  backgroundColor: [
+                    'red',
+                    'red',
+                    'red',
+                    'red',
+                    'red',
+                    'red',
+                    'red'
+                  ],
+                  fill: false
+                },
+                {
+                  data: temp_min,
+                  backgroundColor: [
+                    '#00fff',
+                    '#00fff',
+                    '#00fff',
+                    '#00fff',
+                    '#00fff',
+                    '#00fff',
+                    '#00fff'
+                  ],
+                  fill: false
+                },
+              ]
+            },
+            options: {
+              legend: {
+                display: false
+              },
+              scales: {
+                xAxes: [
+                  {
+                    display: true
+                  }
+                ],
+                yAxes: [
+                  {
+                    display: true
+                  }
+                ]
+              }
+            }
+          });
+      });
+    });
+  }
 }
+ 
---
+++ src/app/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private _http: HttpClient) {}
+
+  dailyForecast(){
+    return this._http.get("https://samples.openweathermap.org/data/2.5/forecast/daily?q=M%C3%BCnchen,DE&appid=b6907d289e10d714a6e88b30761fae22");
+  }
+}
